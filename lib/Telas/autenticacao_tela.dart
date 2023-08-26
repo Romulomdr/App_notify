54e6e71573f56347101201007af71176b1bfd1ec
romulomdr <romulomatheus31@gmail.com>
2023-08-25 21:40:08 -0300

    Autentication login 2.0

--- lib/Telas/autenticacao_tela.dart
+++ lib/Telas/autenticacao_tela.dart
@@ -28,61 +28,65 @@ bool queroEntrar = true;
           Padding(
             padding: const EdgeInsets.all(16.0),
             child: Form(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Center(child: Image.asset("assets/logo.png", height: 128)),
-                  const Text(
-                    "Notify App",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      fontSize: 48,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 32,
-                  ),
-                  TextFormField(decoration: const InputDecoration(
-                    label: Text("E-mail")),
-                  ),
-                  TextFormField(decoration: const InputDecoration(
-                    label: Text("Senha")),
-                    obscureText: true,
-                  ),
-                  Visibility(
-                    visible: !queroEntrar,
-                    child: Column(
-                      children: [
-                        TextFormField(decoration: const InputDecoration(
-                        label: Text("Confirme Senha")),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Center(child: Image.asset("assets/logo.png", height: 128)),
+                      const Text(
+                        "Notify App",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 32,
+                      ),
+                      TextFormField(decoration: const InputDecoration(
+                        label: Text("E-mail")),
+                      ),
+                      TextFormField(decoration: const InputDecoration(
+                        label: Text("Senha")),
                         obscureText: true,
+                      ),
+                      Visibility(
+                        visible: !queroEntrar,
+                        child: Column(
+                          children: [
+                            TextFormField(decoration: const InputDecoration(
+                            label: Text("Confirme Senha")),
+                            obscureText: true,
+                            ),
+                            TextFormField(decoration: const InputDecoration(
+                            label: Text("Nome")),
+                            ),
+                          ],
                         ),
-                        TextFormField(decoration: const InputDecoration(
-                        label: Text("Nome")),
-                        ),
-                      ],
-                    ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      ElevatedButton(
+                        onPressed: (){}, 
+                        child: Text((queroEntrar)? "Entrar" : "Cadastrar")
+                      ),
+                      const Divider(),
+                      TextButton(
+                        onPressed: (){
+                          setState(() {
+                            queroEntrar = !queroEntrar;
+                          });
+                        }, 
+                        child: const Text("Ainda não tem uma conta? Cadastre-se")
+                      )
+                    ],
                   ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  ElevatedButton(
-                    onPressed: (){}, 
-                    child: Text((queroEntrar)? "Entrar" : "Cadastrar")
-                  ),
-                  const Divider(),
-                  TextButton(
-                    onPressed: (){
-                      setState(() {
-                        queroEntrar = !queroEntrar;
-                      });
-                    }, 
-                    child: const Text("Ainda não tem uma conta? Cadastre-se")
-                  )
-                ],
+                ),
               ),
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notif_project/Telas/exercicio_telas.dart';
+import 'package:notif_project/Telas/autenticacao_tela.dart';
 
   void main(){
     runApp(const MyApp());
@@ -10,8 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: ExercicioTela(),
+    return const MaterialApp(
+      home: AutenticacaoTela(),
     );
   }
 }
